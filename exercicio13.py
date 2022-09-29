5084f6ee5a11c5fb957b884daa954342d4a212a3
Romulo Lobato <romulo.lobato28@gmail.com>
2022-09-29 18:31:58 -0300

    Create exercicio13.py

---
+++ exercicio13.py
@@ -0,0 +1,6 @@
+#leia uma distância em quilômetros e apresente-a convertida em milhas
+
+km = float(input("digite a distância em quilômetros\n"))
+milhas = km/1.61
+
+print(f"a distância em milhas é:{round(milhas,2)} milhas")
