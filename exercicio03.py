d0c947d9e209a692cd10a7e8022fdadc60afed57
Romulo Lobato <romulo.lobato28@gmail.com>
2022-09-29 18:21:27 -0300

    renomes

---
+++ exercicio03.py
@@ -0,0 +1,8 @@
+#peça ao usuário que digite 3 números e imprima a soma deles
+
+soma = 0
+for i in range(3):
+    num = float(input("digite um número\n"))
+    soma += num
+
+print(f"a soma dos valores digitados foi:{soma}")
---
+++ exercicio11.py
@@ -0,0 +1,8 @@
+#leia a velocidade em km/h (quilômetros por hora) e apresente-a convertida em m/s (metros por segundo).
+
+# K = km/h e M = M/s
+
+K = float(input("Digite a velocidade em Km/h(quilometros por hora)\n"))
+M = K/3.6
+
+print(f"A velocidade em metros por segundo é: {M} m/s")
