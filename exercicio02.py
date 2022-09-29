e5bca24c684b307909e7257d350ad6f158ec6e32
Romulo Lobato <romulo.lobato28@gmail.com>
2022-09-29 16:24:55 -0300

    Create exercicio02.py
    segundo exercício

---
+++ exercicio02.py
@@ -0,0 +1,5 @@
+#faça um programa que leia um número real e o imprima
+
+num_real = float(input("digite um número\n"))
+#lembrete: No python, um número com ponto flutuante ten esta cara:"10.5" e não :"10,5", ou seja, usa-se um ponto ao invés de vírgula
+print(f"o número digitado foi:{num_real}")
