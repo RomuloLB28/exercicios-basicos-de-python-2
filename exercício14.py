a14160b31d81d0b0da3e7ad68173e012353447b8
Romulo Lobato <romulo.lobato28@gmail.com>
2022-09-29 18:42:34 -0300

    Create exercício14.py

---
+++ exercício14.py
@@ -0,0 +1,6 @@
+#Leia ângulo em graus e apresente-o convvertido em radianos.
+
+graus = float(input("Digite o o valor do ângulo em graus\n"))
+radianos = graus*(3.14/180)
+
+print(f" o valor do ângulo em radianos é: {round(radianos,2)}")
